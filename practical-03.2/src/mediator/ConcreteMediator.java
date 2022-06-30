7bda25a3c7c7bf6752e9ddd70628d6066b5c66d6
fhildeb <felix@lukso.io>
2022-06-30 23:21:53 +0200

    add mediator

---
+++ practical-03.2/src/mediator/ConcreteMediator.java
@@ -0,0 +1,23 @@
+package mediator;
+
+public class ConcreteMediator implements IMediator{
+
+	@Override
+	public void register(Participant participant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void send(String from, String to, String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendToAll(String from, String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
